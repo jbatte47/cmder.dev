d46ff63e6c473f1ffdb7f47b8efa096818f1b55c
John Batte <john.batte@icainformatics.com>
2014-07-25 13:24:51 -0500

    package contents

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'battecode.cmder.dev'
+$url = 'https://github.com/jbatte47/cmder.dev/releases/download/1.1.3-zeta/cmder.7z'
+
+try { 
+  $installDir = "$(Join-Path $env:USERPROFILE AppData\Local\cmder)" 
+  
+  if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
+  
+  $tempDir = "$env:TEMP\chocolatey\$($packageName)"
+  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+
+  $file = Join-Path $tempDir "$($packageName).7z"
+  Get-ChocolateyWebFile "$packageName" "$file" "$url"
+
+  Start-Process "7za" -ArgumentList "x -o`"$installDir`" -y `"$file`"" -Wait
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
